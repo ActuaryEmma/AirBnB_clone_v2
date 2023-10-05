5bdf4d33c6ee552bf312589b8bb9f03214bdf947
ActuaryEmma <emmanyakio@gmail.com>
2023-10-05 21:00:33 +0300

    Task 1 update

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,6 +3,8 @@
 Fabric script that generates a .tgz archive from the contents
 of the web_static folder of your AirBnB Clone repo, using the function do_pack.
 """
+
+
 from fabric.api import local
 from datetime import datetime
 
