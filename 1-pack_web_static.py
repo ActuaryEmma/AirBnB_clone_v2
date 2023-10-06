189f9a36451364056ca9b444cb894c3ba5eff672
ActuaryEmma <emmanyakio@gmail.com>
2023-10-06 17:38:06 +0300

    added try.. except block

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -17,12 +17,15 @@ def do_pack():
     print : web_static_<year><month><day><hour><minute><second>.tgz
     """
 
-    date_now = datetime.now()
-    timestamp = date_now.strftime("%Y%m%d%H%M%S")
+    try:
+        date_now = datetime.now()
+        timestamp = date_now.strftime("%Y%m%d%H%M%S")
 
-    local("mkdir -p versions")
+        local("mkdir -p versions")
 
-    archive_name = "versions/web_static_{}.tgz".format(timestamp)
-    local("tar -czvf {} web_static".format(archive_name))
+        archive_name = "versions/web_static_{}.tgz".format(timestamp)
+        local("tar -czvf {} web_static".format(archive_name))
 
-    return archive_name
+        return archive_name
+    except Exception:
+        return None
