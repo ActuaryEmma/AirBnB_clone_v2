74575451a786a899b67f4d4c0a55ef605d134be6
ActuaryEmma <emmanyakio@gmail.com>
2023-10-06 17:24:15 +0300

    task 0 shellcheck"

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,9 +12,6 @@ rm /data/web_static/current
 ln -s /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 config_file="/etc/nginx/sites-available/default"
-nginx_config="location /hbnb_static/ {
-    alias /data/web_static/current/;
-}"
 sed -i "/server_name _;/a\\        location /hbnb_static/ {alias /data/web_static/current/;}" "$config_file" > /dev/null
 # sed -i "/server_name _;/a $nginx_config" "$config_file"
 service nginx restart
