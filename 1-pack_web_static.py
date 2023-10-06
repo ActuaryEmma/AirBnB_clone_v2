a5c609331c9acd07756dcfad7776e70d57abaa11
ActuaryEmma <emmanyakio@gmail.com>
2023-10-06 17:32:16 +0300

    added method documention

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-from fabric.api import local
-from datetime import datetime
 """
 Fabric script that generates a .tgz archive from the contents
 of the web_static folder of your AirBnB Clone repo, using the function do_pack.
 local - run shell commands on the local machine
 datetime - the module have date and time
 """
+from fabric.api import local
+from datatime import datetime
 
 
 def do_pack():
+    """
+    date - return the current date and time
+    local - run shell commands on the local machine
+    archive files are stored in versions in folder
+    print : web_static_<year><month><day><hour><minute><second>.tgz
+    """
+
     date_now = datetime.now()
     timestamp = date_now.strftime("%Y%m%d%H%M%S")
 
