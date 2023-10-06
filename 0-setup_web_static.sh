4b0bacd08c196131092ec1ee86f618bcb28a7098
ActuaryEmma <emmanyakio@gmail.com>
2023-10-06 16:56:01 +0300

    Task 0 update: added server_name under /etc/nginx/sites-enabled/default

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
 # sets up web servers for the deployment of web_static
 
-sudo apt -y update
-sudo apt install -y nginx
-sudo service nginx start
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared
-sudo mkdir -p /data/web_static/releases/test/
-echo "Test data for task 1" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-sudo rm /data/web_static/current
-sudo ln -s /data/web_static/releases/test /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+apt -y update
+apt install -y nginx
+service nginx start
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/releases/test/
+echo "Test data for task 1" > /data/web_static/releases/test/index.html
+rm /data/web_static/current
+ln -s /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data/
 config_file="/etc/nginx/sites-available/default"
 nginx_config="location /hbnb_static/ {
     alias /data/web_static/current/;
 }"
-if ! grep -q "$nginx_config" "$config_file"; then
-	sudo sed -i "/server_name _;/a $nginx_config" "$config_file"
-fi
-
-sudo service nginx restart
-exit 0
+sed -i "/server_name _;/a\\        location /hbnb_static/ {alias /data/web_static/current/;}" "$config_file" > /dev/null
+# sed -i "/server_name _;/a $nginx_config" "$config_file"
+service nginx restart
